48f292e7bda6cb557a69c9054c10a8ba118e43f4
danya2 <90099519+DanyaHACKED@users.noreply.github.com>
2022-09-12 21:48:12 +0800

    Add files via upload

---
+++ MainScript.lua
@@ -0,0 +1,7 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("FalixWare Hub", "Sentinel")
+local falixware = Window:NewTab("Render")
+local falixsection = falixware:NewSection("Render")
+falixsection:NewButton("Watermark", "watermark for roblox", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DanyaHACKED/falixwaretest/main/Render/watermark.lua", true))()
+end)
