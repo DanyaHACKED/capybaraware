7316aec3573bcfe94d4c73593d260ca9ff88ae4c
danya2 <90099519+DanyaHACKED@users.noreply.github.com>
2022-10-02 01:54:06 +0800

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -9,8 +9,10 @@ OrionLib:MakeNotification({
 	Time = 15
 })
 
-local falixware = Window:NewTab("Render")
-local falixsection = falixware:NewSection("Render")
+local render = Window:NewTab("Render")
+local movement = Window:NewTab("Movement")
+local falixsection = render:NewSection("Render")
+local movementsection = movement:NewSection("Render")
 falixsection:NewButton("Watermark", "watermark for roblox", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DanyaHACKED/falixwaretest/main/Render/watermark.lua", true))()
 end)
@@ -22,4 +24,24 @@ falixsection:NewButton("ESP", "ESP for roblox", function()
 end)
 falixsection:NewKeybind("UI", "Keybinds UI", Enum.KeyCode.F, function()
 	Library:ToggleUI()
+end)
+movement:NewButton("Infinite Jump", "Jump better", function()
+local Player = game:GetService'Players'.LocalPlayer;
+local UIS = game:GetService'UserInputService';
+
+_G.JumpHeight = 50;
+
+function Action(Object, Function) if Object ~= nil then Function(Object); end end
+
+UIS.InputBegan:connect(function(UserInput)
+    if UserInput.UserInputType == Enum.UserInputType.Keyboard and UserInput.KeyCode == Enum.KeyCode.Space then
+        Action(Player.Character.Humanoid, function(self)
+            if self:GetState() == Enum.HumanoidStateType.Jumping or self:GetState() == Enum.HumanoidStateType.Freefall then
+                Action(self.Parent.HumanoidRootPart, function(self)
+                    self.Velocity = Vector3.new(0, _G.JumpHeight, 0);
+                end)
+            end
+        end)
+    end
+end)
 end)
