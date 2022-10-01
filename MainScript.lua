f7065f46212153b1e68d38a3dd1caed5a9439d7e
danya2 <90099519+DanyaHACKED@users.noreply.github.com>
2022-10-02 02:48:09 +0800

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -22,26 +22,10 @@ end)
 falixsection:NewButton("ESP", "ESP for roblox", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DanyaHACKED/falixwaretest/main/Render/esp", true))()
 end)
+movementsection:NewButton("Infinite Jump", "Jumpping better", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DanyaHACKED/falixwaretest/main/Movement/infjump", true))()
+end)
 falixsection:NewKeybind("UI", "Keybinds UI", Enum.KeyCode.F, function()
 	Library:ToggleUI()
 end)
-movement:NewButton("Infinite Jump", "Jump better", function()
-local Player = game:GetService'Players'.LocalPlayer;
-local UIS = game:GetService'UserInputService';
 
-_G.JumpHeight = 50;
-
-function Action(Object, Function) if Object ~= nil then Function(Object); end end
-
-UIS.InputBegan:connect(function(UserInput)
-    if UserInput.UserInputType == Enum.UserInputType.Keyboard and UserInput.KeyCode == Enum.KeyCode.Space then
-        Action(Player.Character.Humanoid, function(self)
-            if self:GetState() == Enum.HumanoidStateType.Jumping or self:GetState() == Enum.HumanoidStateType.Freefall then
-                Action(self.Parent.HumanoidRootPart, function(self)
-                    self.Velocity = Vector3.new(0, _G.JumpHeight, 0);
-                end)
-            end
-        end)
-    end
-end)
-end)
